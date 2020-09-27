a0652c7e8313496d17912a044cea169f124b9efd
diyarfaraj <diyar.faraj@gmail.com>
2020-09-27 20:36:54 +0200

    initial commit 2

---
+++ mentor-app-api/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using mentor_app_api.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mentor_app_api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext( DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
