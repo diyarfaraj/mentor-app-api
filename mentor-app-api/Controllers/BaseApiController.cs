f7cf08a3607c3993dff75deed17c3b93e70df6f8
diyarfaraj <diyar.faraj@gmail.com>
2020-10-01 22:06:29 +0200

    login issue solved

---
+++ mentor-app-api/Controllers/BaseApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mentor_app_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+
+    }
+}
